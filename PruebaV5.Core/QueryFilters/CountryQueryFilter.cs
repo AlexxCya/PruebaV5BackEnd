34113c8c0c44674026041dc2106b5f926e41f706
Alex Jonguitud <ajonguituddelangel@suppliers.tenaris.com>
2021-06-16 20:30:42 -0500

    AJDA: Refactor in QueryFillter

--- PruebaV5.Core/QueryFilters/CountryQueryFilter.cs
+++ PruebaV5.Core/QueryFilters/CountryQueryFilter.cs
@@ -4,11 +4,8 @@ using System.Text;
 
 namespace PruebaV5.Core.QueryFilters
 {
-    public class CountryQueryFilter
+    public class CountryQueryFilter: GlobalQueryFilter
     {
-        public string Name { get; set; }
         public string Alpha2Code { get; set; }
-        public int PageSize { get; set; }
-        public int PageNumber { get; set; }
     }
 }
---
+++ PruebaV5.Core/QueryFilters/GlobalQueryFilter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PruebaV5.Core.QueryFilters
+{
+    public abstract class GlobalQueryFilter
+    {
+        public string Name { get; set; }
+        public int PageSize { get; set; }
+        public int PageNumber { get; set; }
+    }
+}
